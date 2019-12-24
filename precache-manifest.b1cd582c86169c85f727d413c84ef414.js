5560762bee4db3cd0bbc791d3a7f60cc308361b1
uwaelpis <ingenioushope@gmail.com>
2019-12-24 03:55:18 +0100

    Updates

---
+++ precache-manifest.b1cd582c86169c85f727d413c84ef414.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8438f2b006d96b4bedb0222a0626efdb",
+    "url": "/ICBECON/index.html"
+  },
+  {
+    "revision": "3e57cbe0735029ffa635",
+    "url": "/ICBECON/static/css/2.0fe53a39.chunk.css"
+  },
+  {
+    "revision": "0f950219fc3010e7e282",
+    "url": "/ICBECON/static/css/main.80e9dff8.chunk.css"
+  },
+  {
+    "revision": "3e57cbe0735029ffa635",
+    "url": "/ICBECON/static/js/2.93dc7db0.chunk.js"
+  },
+  {
+    "revision": "0f950219fc3010e7e282",
+    "url": "/ICBECON/static/js/main.15fadcad.chunk.js"
+  },
+  {
+    "revision": "06af799f60aa25ec2bba",
+    "url": "/ICBECON/static/js/runtime~main.6ecaa337.js"
+  },
+  {
+    "revision": "49644ea78c189ae0225876e9b5596494",
+    "url": "/ICBECON/static/media/key-speaker.49644ea7.jpeg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ICBECON/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ICBECON/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ICBECON/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ICBECON/static/media/slick.f97e3bbf.svg"
+  }
+]);
